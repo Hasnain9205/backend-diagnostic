c99b4da915c2ec9d2e8280580b9579c99dd0bd7c
Md. Hasnain <rajon013039799@gmail.com>
2025-01-06 11:28:42 +0600

    all updated

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ app.use(express.json());
 
 // CORS configuration
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://spontaneous-starship-9b651d.netlify.app",
-  ],
+  origin: ["https://nimble-cat-c9d180.netlify.app"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
