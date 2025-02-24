15767e1a50c371306b8c4e525b489aa0ab5b4a43
Md. Hasnain <rajon013039799@gmail.com>
2025-02-24 09:26:29 +0600

    created prescription add algolia search  successfully

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ app.use(express.json());
 
 // CORS configuration
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://classy-croissant-d591ad.netlify.app",
-  ],
+  origin: ["http://localhost:5173", "https://cute-alpaca-261dbb.netlify.app"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
