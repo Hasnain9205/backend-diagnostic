db9fa8fce666391e8ac744718101a3f316c0106d
Md. Hasnain <rajon013039799@gmail.com>
2025-06-12 19:51:27 +0600

    updated all in diagnostic project

--- server.js
+++ server.js
@@ -26,10 +26,7 @@ app.use(express.json());
 
 // CORS configuration
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://delightful-kulfi-493196.netlify.app",
-  ],
+  origin: ["https://snazzy-bonbon-dc42e3.netlify.app", "http://localhost:5173"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
